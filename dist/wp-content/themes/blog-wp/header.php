fed6929814be027efeeee7169d9f5352543a9ed8
Alexander Nikolaychuk <alexksnikol@gmail.com>
2021-01-18 13:50:33 +0200

    remove old GA

--- dist/wp-content/themes/blog-wp/header.php
+++ dist/wp-content/themes/blog-wp/header.php
@@ -10,6 +10,8 @@
 <html lang="en">
 <head>
 
+
+
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="user-scalable=no, width=device-width, initial-scale=1, maximum-scale=1">
@@ -48,6 +50,7 @@
 </head>
 
 <body>
+
     <div id="preload">
         <span id="preload__loading-line"></span>
     </div>
